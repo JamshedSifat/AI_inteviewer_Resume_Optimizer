8cf2a2fb50b48fcd9ed2841dc88a07d2e363abc8
JamshedSifat <mdjamshedsifat@gmail.com>
2026-09-16 19:07:32 +0600

    fix: update production api baseURL

--- AI_inteviewer_Resume_Optimizer/AI_inteviewer_Resume_Optimizer/src/services/api.js
+++ AI_inteviewer_Resume_Optimizer/AI_inteviewer_Resume_Optimizer/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://ai-inteviewer-resume-optimizer.onrender.com/',
   timeout: 120000, // ২ মিনিট টাইমআউট
   headers: {
     'Content-Type': 'application/json',
